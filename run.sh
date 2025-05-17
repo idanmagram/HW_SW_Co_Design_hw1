1bb5594030e40c6bca4c2b9c6e464c512d641ee6
Luiz Wainstein <luiz.zaimawainstein@gmail.com>
2025-05-17 20:33:59 +0300

    add image size to file name

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ fi
 
 IMAGE_SIZE=$1
 NUM_THREADS=$2
-REPORT_FLAMEGRAPH=1
+# REPORT_FLAMEGRAPH=1
 g++ -Wall -Wextra -O2 -std=c++17 -g -o naive_gaussian_blur gaussian_blur_naive.cpp utils.cpp -DIMAGE_SIZE=$IMAGE_SIZE
 g++ -Wall -Wextra -O2 -std=c++17 -pthread -g -o multithreaded_gaussian_blur gaussian_blur_multithreaded.cpp utils.cpp -DIMAGE_SIZE=$IMAGE_SIZE -DNUM_THREADS=$NUM_THREADS
 g++ -O2 -mavx -mfma -std=c++17 -g -o simd_gaussian_blur gaussian_blur_simd.cpp utils.cpp -DIMAGE_SIZE=$IMAGE_SIZE
@@ -32,31 +32,34 @@ else
 fi
 
 echo "Perf Naive"
-perf record -e cycles,instructions,cache-references,cache-misses -F 1000 -g ./naive_gaussian_blur
+perf record -e cycles,instructions,cache-references,cache-misses -a -F 1000 -g ./naive_gaussian_blur
 if [ -n "$REPORT_FLAMEGRAPH" ]; then
     perf script > naive.perf
     ../Flamegraph/stackcollapse-perf.pl naive.perf > naive.folded
     ../Flamegraph/flamegraph.pl naive.folded > naive_flamegraph.svg
+    # xdg-open naive_flamegraph.svg &
 else
-    perf report > naive_perf.txt
+    perf report > "naive_perf_$(echo $IMAGE_SIZE).txt"
 fi
 
 echo "Perf Multithreaded"
-perf record -e cycles,instructions,cache-references,cache-misses -F 1000 -g ./multithreaded_gaussian_blur
+perf record -e cycles,instructions,cache-references,cache-misses -a -F 1000 -g ./multithreaded_gaussian_blur
 if [ -n "$REPORT_FLAMEGRAPH" ]; then
     perf script > multithreaded.perf
     ../Flamegraph/stackcollapse-perf.pl multithreaded.perf > multithreaded.folded
     ../Flamegraph/flamegraph.pl multithreaded.folded > multithreaded_flamegraph.svg
+    # xdg-open multithreaded_flamegraph.svg &
 else
-    perf report > multithreaded_perf.txt
+    perf report > "multithreaded_perf_$(echo $IMAGE_SIZE)_$(echo $NUM_THREADS).txt"
 fi
 
 echo "Running SIMD"
-perf record -e cycles,instructions,cache-references,cache-misses -F 1000 -g ./simd_gaussian_blur
+perf record -e cycles,instructions,cache-references,cache-misses -a -F 1000 -g ./simd_gaussian_blur
 if [ -n "$REPORT_FLAMEGRAPH" ]; then
     perf script > simd.perf
     ../Flamegraph/stackcollapse-perf.pl simd.perf > simd.folded
     ../Flamegraph/flamegraph.pl simd.folded > simd_flamegraph.svg
+    # xdg-open simd_flamegraph.svg &
 else
-    perf report > simd_perf.txt
+    perf report > "simd_perf_$(echo $IMAGE_SIZE).txt"
 fi
